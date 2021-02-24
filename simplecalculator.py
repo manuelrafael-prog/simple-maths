a1ebedc7b605c2414237ca2b5ac9684fa7d7dcba
github_username <manuelrafaelslt@gmail.com>
2021-02-24 18:04:37 +0800

    Update simplecalculator.py

--- simplecalculator.py
+++ simplecalculator.py
@@ -1,55 +1,56 @@
-# simple maths (https://www.programiz.com/python-programming/examples/calculator)
-# features and improvements will be made later
-
-# addition of two numbers
-def add(x, y):
-    return x + y
-
-# subtraction of two numbers
+def welcome():
+    print('''
+Welcome to Calculator
+''')
 
 
-def subtract(x, y):
-    return x - y
+def calculate():
+    operation = input('''
+Please type in the math operation you would like to complete:
++ for addition
+- for subtraction
+* for multiplication
+/ for division
+''')
 
-# multiplication of two numbers
+    number_1 = float(input('Please enter the first number: '))
+    number_2 = float(input('Please enter the second number: '))
 
+    if operation == '+':
+        print('{} + {} = '.format(number_1, number_2))
+        print(number_1 + number_2)
 
-def multiply(x, y):
-    return x * y
+    elif operation == '-':
+        print('{} - {} = '.format(number_1, number_2))
+        print(number_1 - number_2)
 
-# division of two numbers
+    elif operation == '*':
+        print('{} * {} = '.format(number_1, number_2))
+        print(number_1 * number_2)
 
+    elif operation == '/':
+        print('{} / {} = '.format(number_1, number_2))
+        print(number_1 / number_2)
 
-def divide(x, y):
-    return x / y
-
-
-print("Select operation.")
-print("1.Add")
-print("2.Subtract")
-print("3.Multiply")
-print("4.Divide")
-
-while True:
-    # Take input from the user
-    choice = input("Enter choice(1/2/3/4): ")
-
-    # Check if choice is one of the four options
-    if choice in ('1', '2', '3', '4'):
-        num1 = float(input("Enter first number: "))
-        num2 = float(input("Enter second number: "))
-
-        if choice == '1':
-            print(num1, "+", num2, "=", add(num1, num2))
-
-        elif choice == '2':
-            print(num1, "-", num2, "=", subtract(num1, num2))
-
-        elif choice == '3':
-            print(num1, "*", num2, "=", multiply(num1, num2))
-
-        elif choice == '4':
-            print(num1, "/", num2, "=", divide(num1, num2))
-        break
     else:
-        print("Invalid Input")
+        print('You have not typed a valid operator, please run the program again.')
+
+    # Add again() function to calculate() function
+    again()
+
+
+def again():
+    calc_again = input('''
+Do you want to calculate again?
+Please type Y for YES or N for NO.
+''')
+
+    if calc_again.upper() == 'Y':
+        calculate()
+    elif calc_again.upper() == 'N':
+        print('See you later.')
+    else:
+        again()
+
+
+calculate()
